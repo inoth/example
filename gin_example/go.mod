05248c30b1bb4db90b9d7c592078e433b50b088e
inoth <inoth@DESKTOP-JU9AJC0>
2021-12-23 14:51:50 +0800

    gin 片段

--- gin_example/go.mod
+++ gin_example/go.mod
@@ -2,4 +2,7 @@ module gin-example
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.7
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/google/uuid v1.3.0
+)
--- gin_example/main.go
+++ gin_example/main.go
@@ -1,6 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type UserState struct {
+	Uid  string
+	Name string
+}
 
 func main() {
 	r := gin.New()
@@ -29,6 +40,12 @@ func main() {
 	// 添加一个中间件
 	r.Use(func(c *gin.Context) {
 		// do something
+		ctx := context.WithValue(c.Request.Context(), "RequestId", randomId())
+		c.Set("RequestId", ctx)
+		c.Set("UserState", UserState{
+			Uid:  "123123",
+			Name: "inoth",
+		})
 		c.Next()
 	})
 
@@ -46,5 +63,31 @@ func main() {
 }
 
 func HelloWorld(c *gin.Context) {
+	// 虚假的链路跟踪方式，使用context参数
+	ctx := GetRequestId(c)
+	fmt.Printf("外部 %v\n", ctx.Value("RequestId"))
+	go func(context.Context) {
+		fmt.Printf("groutene 内部 %v\n", ctx.Value("RequestId"))
+	}(ctx)
+
+	// 获取登录状态中的用户信息
+	userState := GetUserState(c)
+	fmt.Printf("%v:%v\n", userState.Uid, userState.Name)
+
 	c.String(200, "hello world")
+}
+
+func randomId() string {
+	id, _ := uuid.NewUUID()
+	return id.String()[:8]
+}
+
+func GetRequestId(c *gin.Context) context.Context {
+	ctx, _ := c.Get("RequestId")
+	return ctx.(context.Context)
+}
+
+func GetUserState(c *gin.Context) UserState {
+	userState, _ := c.Get("UserState")
+	return userState.(UserState)
 }
